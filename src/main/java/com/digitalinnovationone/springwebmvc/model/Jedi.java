27ef478872118104364cbe3c2eaf14ea242f40c5
alexandre <amenezes031@gmail.com>
2021-01-14 14:42:00 -0300

    Create Jedi model

---
+++ src/main/java/com/digitalinnovationone/springwebmvc/model/Jedi.java
@@ -0,0 +1,32 @@
+package com.digitalinnovationone.springwebmvc.model;
+
+public class Jedi {
+
+	private String firstName;
+	private String lastName;
+
+	public Jedi() {
+	}
+
+	public Jedi(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
